24bc9e96f2c1abb8c66fd340e3bc15d7301875f9
masaibar <orebatemoko@gmail.com>
2016-05-04 22:05:18 +0900

    insert用、select用の画面を追加(仮)

--- app.py
+++ app.py
@@ -7,8 +7,21 @@ from bottle import TEMPLATE_PATH, jinja2_template as template
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH.append(BASE_DIR + "/views")
 
-@route('/')
+
+@route("/")
+@route("/index")
 def hello():
-    return template('index', message = "Hello World!!!!!!")
+    return template("index", message="Hello World!!!!!!")
+
+
+@route("/insert")
+def insert():
+    return template("insert", message="hoge")
+
+
+@route("/select")
+def select():
+    return template("select", message="select")
+
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
