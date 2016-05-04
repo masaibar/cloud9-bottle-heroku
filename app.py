2344300bfaa009e9d08b7c92f9e73eeac7774469
masaibar <orebatemoko@gmail.com>
2016-05-04 21:55:04 +0900

    テンプレートエンジンにjinja2を使うように変更

--- app.py
+++ app.py
@@ -2,9 +2,13 @@
 
 import os
 from bottle import route, run
+from bottle import TEMPLATE_PATH, jinja2_template as template
 
-@route('/hello/')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_PATH.append(BASE_DIR + "/views")
+
+@route('/')
 def hello():
-    return "Hello World"
+    return template('index', message = "Hello World!!!!!!")
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
